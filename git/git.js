43acbab0ad832b58bcd203eee663babd9489350e
kim, jiwon <wo41896804@gmail.com>
2021-11-05 22:01:10 +0900

    git .js 수정
    미완성
    +css 수정

--- git/git.js
+++ git/git.js
@@ -1,18 +1,43 @@
 // list click
+const listTooltip = document.querySelector("#listTooltip");
+const HOVER_CLASS = "listTooltipChecked";
+
 function clickList() {
-    document.getElementById("dropdown").classList.toggle("show");
+  document.getElementById("dropdown").classList.toggle("show");
+
+
 }
+/* 
+내가 하고 싶은 것
+목록 리스트가 보여졌을 때 목록보기의 툴팁이 보이지 않고 싶다. 
 
-window.onclick = function(event) {
+*/
+window.onclick = (event) => {
+  listTooltip.addEventListener("hover", handleHover);
   if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    const currentClass = listTooltip.className;
+    if (currentClass !== HOVER_CLASS) {
+      listTooltip.className = HOVER_CLASS;
+    } else {
+      listTooltip.className = "";
+    }
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
   }
 }
+
+function handleHover() {
+  const currentClass = listTooltip.className;
+  if (currentClass !== HOVER_CLASS) {
+    listTooltip.className = HOVER_CLASS;
+  } else {
+    listTooltip.className = "";
+  }
+
+}
+
