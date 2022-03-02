1619d77d1a0ad584909b58b1eda56a0c94915aaf
kim, jiwon <wo41896804@gmail.com>
2022-03-02 19:09:59 +0900

    studypage

---
+++ pages/study.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import Intro from '../components/study/Intro';
+import indexStyles from '../css/index.module.css';
+
+const study = () => {
+    return (
+      <div className={indexStyles.index}>
+            <Nav />
+          <Intro />
+      </div>
+    );
+};
+
+export default study;
