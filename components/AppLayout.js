dd50f4f41c9f3f3b1c7d96834cb79de55232654c
kim, jiwon <wo41896804@gmail.com>
2022-04-14 15:39:21 +0900

    .gitignore + applayout

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -2,14 +2,14 @@ import React from "react";
 import propTypes from "prop-types";
 import Nav from "./Nav";
 import layoutStyle from '../css/appLayout.module.css'
-import Menu from "./Menu";
+// import Menu from "./Menu";
+// import Contents from "./Contents/Contents";
 
 const AppLayout = ({ children }) => {
   return (
     <div className={layoutStyle.appLayout}>
       <Nav className={layoutStyle.nav} />
       <div className={layoutStyle.contents}>
-        {/* <Menu /> */}
         <div className={layoutStyle.tab}>{children}</div>
       </div>
     </div>
---
+++ static.js
@@ -0,0 +1,11 @@
+// export const COLOR = rgb(198, 245, 29);
+
+export const POST_CATEGORT = [
+    'react',
+    'regExp',
+    'git',
+    'etc',
+    'error',
+    'ubuntu',
+    'unix',
+];
